5d34b8d5bfce0e2eac4e76e0c638d4247296c43b
Florin Ercus <florin.ercus@loopline-systems.com>
2021-05-29 23:14:16 +0200

    fixed url tests

---
+++ src/cli/app.rs
@@ -0,0 +1,64 @@
+use clap::{
+    App,
+    AppSettings,
+    Arg,
+    app_from_crate,
+    crate_name,
+    crate_version,
+    crate_authors,
+    crate_description,
+};
+use crate::cli::input::{validate_natural_one_plus, validate_min_rating};
+
+pub fn clap_app() -> App<'static, 'static> {
+    app_from_crate!()
+        .global_setting(AppSettings::NextLineHelp)
+        .global_setting(AppSettings::ColoredHelp)
+        .subcommand(
+            App::new("list")
+                .about("lists movies")
+                .args(&[
+                    Arg::with_name("limit").short("l").long("limit").takes_value(true)
+                        .default_value("50")
+                        .help("The limit of results per page that has been set")
+                        .long_help("The limit of results per page that has been set.\n\
+                        Integer between 1 - 50 (inclusive)"),
+                    Arg::with_name("page")
+                        .takes_value(true).short("p").long("page")
+                        .help("The page in the list of movies")
+                        .long_help("The page in the list of movies\n\
+                        e.g. limit=15 and page=2 will show you movies 15-30\n\
+                        Integer (Unsigned)")
+                        .validator(validate_natural_one_plus("should be 1 or more".to_string())),
+                    Arg::with_name("quality").long("quality").takes_value(true)
+                        .case_insensitive(true)
+                        .possible_values(&["720p", "1080p", "2160p", "3D"])
+                        .help("Filter by a given quality")
+                        .long_help("Filter by a given quality\nString (720p, 1080p, 2160p, 3D)"),
+                    Arg::with_name("rating")
+                        .takes_value(true).short("r").long("rating")
+                        .help("Filter movie by a given minimum IMDb rating")
+                        .long_help("Filter movie by a given minimum IMDb rating\nInteger between 0 - 9 (inclusive)")
+                        .validator(validate_min_rating("this should be an integer between 1 and 9".to_string())),
+                    Arg::with_name("query").takes_value(true).short("q").long("query")
+                        .help("Search query")
+                        .long_help("Search query, matching on: Movie Title/IMDb Code, Actor Name/IMDb Code, Director Name/IMDb Code"),
+                    Arg::with_name("genre").takes_value(true).short("g").long("genre")
+                        .help("Filter by a given genre")
+                        .long_help("Filter by a given genre (See http://www.imdb.com/genre/ for full list)"),
+                    Arg::with_name("sort").long("sort").short("s").takes_value(true)
+                        .case_insensitive(true)
+                        .possible_values(&["title", "year", "rating", "peers", "seeds", "download_count", "like_count", "date_added"])
+                        .help("Sorts the results by a criteria")
+                        .long_help("Sorts the results by a criteria\nString (title, year, rating, peers, seeds, download_count, like_count, date_added)"),
+                    Arg::with_name("order").long("order").short("o").takes_value(true)
+                        .case_insensitive(true)
+                        .possible_values(&["desc", "asc"])
+                        .help("Order the results ascending or descending")
+                        .long_help("Order the results ascending or descending. String. 'asc' or 'desc'."),
+                    Arg::with_name("with_rotten_tomatoes").long("rt")
+                        .help("get rotten tomatoes ratings")
+                        .long_help("Return the list with the Rotten Tomatoes rating included"),
+                ])
+        )
+}
--- src/cli/input.rs
+++ src/cli/input.rs
@@ -1,4 +1,6 @@
 use strum::{EnumString, EnumVariantNames, Display, EnumIter};
+use clap::ArgMatches;
+use url::Url;
 
 #[derive(Display, Debug, PartialEq, EnumString, EnumVariantNames, EnumIter)]
 pub enum Qualities {
@@ -23,5 +25,77 @@ pub fn validate_min_rating(msg: String) -> impl Fn(String) -> Result<(), String>
     move |val| match val.trim().parse::<u8>() {
         Ok(x) if (1..=9).contains(&x) => Ok(()),
         _ => Err(msg.clone()),
+    }
+}
+
+/// Builds an url for the call described here https://yts.mx/api#list_movies
+pub fn get_list_url(matches: &ArgMatches) -> Url {
+    let mut url = Url::parse("https://yts.mx/api/v2/list_movies.json").unwrap();
+
+    if let Some(limit) = matches.value_of("limit") {
+        url.query_pairs_mut().append_pair("limit", limit);
+    }
+
+    if let Some(val) = matches.value_of("page") {
+        url.query_pairs_mut().append_pair("page", val);
+    }
+
+    if let Some(val) = matches.value_of("quality") {
+        url.query_pairs_mut().append_pair("quality", val);
+    }
+
+    if let Some(val) = matches.value_of("rating") {
+        url.query_pairs_mut().append_pair("minimum_rating", val);
+    }
+
+    if let Some(val) = matches.value_of("query") {
+        url.query_pairs_mut().append_pair("query_term", val);
+    }
+
+    if let Some(val) = matches.value_of("genre") {
+        url.query_pairs_mut().append_pair("genre", val);
+    }
+
+    if let Some(val) = matches.value_of("sort") {
+        url.query_pairs_mut().append_pair("sort_by", val);
+    }
+
+    if let Some(val) = matches.value_of("order") {
+        url.query_pairs_mut().append_pair("order_by", val);
+    }
+
+    if matches.is_present("with_rotten_tomatoes") {
+        url.query_pairs_mut().append_key_only("with_rt_ratings");
+    }
+
+    url
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_list_url;
+    use crate::cli::app::clap_app;
+
+    fn url_from_cli_input(vec: Vec<&str>) -> String {
+        let vec = vec!["yts", "list"].into_iter().chain(vec.into_iter()).collect::<Vec<&str>>();
+        get_list_url(
+            clap_app().get_matches_from(vec)
+                .subcommand_matches("list").unwrap()
+        ).to_string()
+    }
+
+    #[test]
+    fn limit() {
+        assert_eq!(url_from_cli_input(vec!["-l", "14", "-p", "13"]), "https://yts.mx/api/v2/list_movies.json?limit=14&page=13");
+    }
+
+    #[test]
+    fn rotten() {
+        assert_eq!(url_from_cli_input(vec!["--rt"]), "https://yts.mx/api/v2/list_movies.json?limit=50&with_rt_ratings");
+    }
+
+    #[test]
+    fn sort() {
+        assert_eq!(url_from_cli_input(vec!["-s", "title"]), "https://yts.mx/api/v2/list_movies.json?limit=50&sort_by=title");
     }
 }
