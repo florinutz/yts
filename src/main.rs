baa1eb6dc640aabed5d10a484f2af47d4f0cb0e4
florinutz <florin.ercus@loopline-systems.com>
2021-06-10 13:49:07 +0200

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod cli;
 
-use crate::cli::{app::clap_app, input::get_list_url};
-use log::{debug, error};
+use crate::cli::{app::clap_app, input::list_req_from_clap};
+use log::error;
 use std::process::exit;
-use yts::get_list;
+use yts::parse::api::ListResponse;
 
 fn main() {
     env_logger::init();
@@ -12,21 +12,24 @@ fn main() {
 
     // the list subcommand
     if let Some(list_matches) = matches.subcommand_matches("list") {
-        let list = if list_matches.is_present("response-mock-file") {
+        let list: ListResponse = if list_matches.is_present("response-mock-file") {
             let json =
                 std::fs::read_to_string(list_matches.value_of("response-mock-file").unwrap())
                     .expect("can't read mock json data");
             serde_json::from_str(json.as_str()).expect("expected a parsed response")
         } else {
-            let url = get_list_url(list_matches);
-            debug!("url: {}", url);
-            get_list(&url).unwrap_or_else(|e| {
+            let request = list_req_from_clap(list_matches).unwrap_or_else(|e| {
+                eprintln!("encountered a problem while retrieving the list");
+                error!("can't retrieve list: {}", e);
+                exit(1);
+            });
+            request.execute().unwrap_or_else(|e| {
                 eprintln!("encountered a problem while retrieving the list");
                 error!("can't retrieve list: {}", e);
                 exit(1);
             })
         };
 
-        println!("{}", &list);
+        println!("{}", list);
     }
 }
